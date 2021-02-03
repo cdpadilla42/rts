e6509f855c5601b5e7b261d2b0792f0cbc1883f9
Chris Padilla <cdpadilla42@gmail.com>
2021-02-03 11:00:47 -0600

    Add functionality to form submit

--- src/components/AddItem.tsx
+++ src/components/AddItem.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ItemInterface from '../types/Item.interface';
 import ItemsArray from '../types/ItemsArray.type';
 
 type Props = {
@@ -6,7 +7,30 @@ type Props = {
 };
 
 const AddItem = ({ setItems }: Props) => {
-  return <div>hello!</div>;
+  const [name, setName] = useState('King Cake');
+
+  const handleChange = (e: React.FormEvent<HTMLInputElement>): void => {
+    setName(e.currentTarget.value);
+  };
+
+  const handleSubmit = (e: React.SyntheticEvent): void => {
+    e.preventDefault();
+    const newItem: ItemInterface = { name, checked: false };
+    setItems((prevState) => [...prevState, newItem]);
+    setName('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="name"
+        id="name"
+        value={name}
+        onChange={handleChange}
+      />
+    </form>
+  );
 };
 
 export default AddItem;
